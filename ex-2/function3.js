ad5320dbeab5699d15534481377ce86c2105f4c2
federicaarosio <arosio.fede@gmail.com>
2024-04-11 15:56:53 +0200

    js - function3 b

--- ex-2/function3.js
+++ ex-2/function3.js
@@ -11,12 +11,26 @@ const oggetti = [
     { nome: 'Sara', lavoro: 'insegnante' },
     { nome: 'Marta', lavoro: 'medico' },
     { nome: 'Luca', lavoro: 'avvocato' }
-  ];
-  
-  const listaLavori = filterArrayByKey(oggetti, 'lavoro');
-  console.log(listaLavori);
+];
+
+const listaLavori = filterArrayByKey(oggetti, 'lavoro');
+console.log(listaLavori);
 
 
 // Variante: funzione con input array e una stringa, ritornare un nuovo array con gli
 // indici dei soli oggetti (contenuti nell’array) che hanno una chiave con valore uguale
 // alla stringa passata
+
+
+function filterArrayByValue(array, value) {
+    let result = [];
+
+    for (let i = 0; i < array.length; i++) {
+        const obj = array[i];
+        if (Object.values(obj).includes(value)) {
+            result.push(i);
+        }
+    }
+
+    return result;
+}
